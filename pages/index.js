b362b54b3195209323ad77131ec43754f55a19e4
marquesbelem <camilambdasilva@gmail.com>
2021-01-27 16:13:37 -0300

    colocando informações na tela inicial

--- pages/index.js
+++ pages/index.js
@@ -17,29 +17,27 @@ export default function Home() {
 
         <MenuNavbar />
 
-
-
-        <div class="uk-child-width-1-2@s uk-grid-match" uk-grid>
-          <div>
-            <div class="uk-card uk-card-default uk-card-hover uk-card-body">
-              <h3 class="uk-card-title">Default</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+        <div class="uk-container">
+          <div class="uk-flex uk-flex-column">
+            <div class="uk-animation" tabindex="0">
+              <div class="uk-card uk-card-default uk-card-hover uk-card-body uk-animation-slide-left-medium">
+                <h3 class="uk-card-title">Plataforma que</h3>
+                <p>centraliza ideias de jogos para programadores que tem dificuldade em criar ideias ou não saber por onde começar. </p>
+              </div>
+            </div>
+            <div class="uk-animation" tabindex="0">
+              <div class="uk-card uk-card-default uk-card-hover uk-card-body uk-animation-slide-left-medium">
+                <h3 class="uk-card-title">Você programador</h3>
+                <p>quer desenvolver alguns jogos para aprender mais? esse é o lugar</p>
+              </div>
             </div>
           </div>
         </div>
-
       </main>
 
       <footer>
 
       </footer>
-
-
-      <style jsx>{`
-        #bg {
-          background-image: url("bg1.jpg");
-        }
-      `}</style>
     </div>
   )
 }
