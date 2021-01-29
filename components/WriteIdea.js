e7e6602607dc75585cbaed7731bbbe83fadbb586
marquesbelem <camilambdasilva@gmail.com>
2021-01-29 17:00:15 -0300

    validando se o campo foi preenchido

--- components/WriteIdea.js
+++ components/WriteIdea.js
@@ -4,6 +4,8 @@ import UIkit from "uikit";
 
 const WriteIdea = () => {
 
+    const [msgValidation, setMsgValidation] = useState(null);
+
     const [data, setData] = useState({
         title: '',
         content: ''
@@ -21,11 +23,24 @@ const WriteIdea = () => {
     const handleSubmit = async e => {
         e.preventDefault();
 
+        if (data.content.length <= 0 && data.title.length <= 0) {
+            setMsgValidation("Campos obrigatórios.");
+            return;
+        } else if (data.content.length <= 0) {
+            setMsgValidation("Campo descrição é obrigatório.");
+            return;
+        } else if (data.title.length <= 0) {
+            setMsgValidation("Campo título é obrigatório.");
+            return;
+        } else {
+            setMsgValidation("");
+        }
+
         try {
             const response = await axios.post('/api/postIdeas', data);
             console.log(response);
             console.log(data);
-            
+
             UIkit.notification('Sua ideia foi enviada com sucesso!', 'success');
 
         } catch (error) {
@@ -44,14 +59,16 @@ const WriteIdea = () => {
             <div className="uk-card uk-card-body">
                 <form onSubmit={handleSubmit}>
 
-                    <input className="uk-input uk-margin-bottom" placeholder="Titulo" type="text"
+                    <input className="uk-input uk-margin-bottom" placeholder="Título" type="text"
                         name="title" value={data.title} onChange={handleChange} />
 
-                    <textarea className="uk-textarea" rows="5" placeholder="Ideia .." maxLength="300" type="text " name="content"
+                    <textarea className="uk-textarea" rows="5" placeholder="Descrição ..." maxLength="300" type="text " name="content"
                         value={data.content}
                         onChange={handleChange} />
 
-                    <p className="uk-margin-bottom uk-text-danger uk-text-left"><span>Limite de 300 caracteres.</span></p>
+                    <span className="uk-text-danger uk-text-left uk-margin-bottom">{msgValidation}</span>
+
+                    <p className="uk-margin-bottom uk-text-dark uk-text-right"><span>Limite de 300 caracteres.</span></p>
                     <button className="uk-button uk-button-default" type="submit" >Enviar</button>
                 </form>
             </div>
