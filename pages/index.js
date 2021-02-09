3ab3db06a073ce40b5a963cc4cfcadbfa13dd0fd
marquesbelem <camilambdasilva@gmail.com>
2021-02-09 10:38:59 -0300

    tela de perfil

--- pages/index.js
+++ pages/index.js
@@ -14,23 +14,23 @@ export default function Home() {
       <MenuNavbar />
 
       <div className="uk-container uk-margin-top ptb-70">
-        <div className="uk-child-width-1-3@m uk-flex uk-flex-wrap uk-flex-center" uk-grid="true">
-          <div className="uk-card uk-card-hover uk-card-default uk-card-body uk-animation-slide-bottom uk-margin-right uk-margin-top">
+        <div className="uk-flex uk-flex-wrap uk-flex-center@m" uk-grid="true">
+          <div className="uk-width-1-3@m uk-card uk-card-hover uk-card-default uk-card-body uk-animation-slide-bottom uk-margin-left uk-margin-right uk-margin-top">
             <h3 className="uk-card-title">Plataforma que</h3>
             <p>centraliza ideias de jogos para programadores que tem dificuldade em criar ideias ou não saber por onde começar. </p>
           </div>
 
-          <div className="uk-card uk-card-hover uk-card-default uk-card-body uk-animation-slide-bottom uk-margin-right uk-margin-top">
+          <div className="uk-width-1-3@m uk-card uk-card-hover uk-card-default uk-card-body uk-animation-slide-bottom uk-margin-left uk-margin-right uk-margin-top">
             <h3 className="uk-card-title">Você programador</h3>
             <p>quer desenvolver alguns jogos para aprender mais? esse é o lugar</p>
           </div>
 
-          <div className="uk-card uk-card-hover uk-card-default uk-card-body uk-animation-slide-top uk-margin-right uk-margin-top">
+          <div className="uk-width-1-3@m uk-card uk-card-hover uk-card-default uk-card-body uk-animation-slide-top uk-margin-left uk-margin-right uk-margin-top">
             <h3 className="uk-card-title">Você game desing</h3>
             <p>que tem varias ideias mas não tem ngm para desenvolver? esse é o lugar</p>
           </div>
 
-          <div className="uk-card uk-card-hover uk-card-default uk-card-body uk-animation-slide-top uk-margin-right uk-margin-top">
+          <div className="uk-width-1-3@m uk-card uk-card-hover uk-card-default uk-card-body uk-animation-slide-top uk-margin-left uk-margin-right uk-margin-top">
             <h3 className="uk-card-title">Não é tema</h3>
             <p>que você vai encontrar aqui, é uma ideia, começo, meio e fim com os detalhes necessários.</p>
           </div>
